6e3f9152673aed0ac1a6bbe00556cb2f2efaf04b
adisonzenemij <adisonzenemij@gmail.com>
2024-11-06 22:44:07 -0500

    feat: a1cb3-55dad-ff582

---
+++ a1cb3-55dad-ff582/src/main/java/com/app/project/Software.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.app.project;
+
+/**
+ *
+ * @author adiso
+ */
+public class Software {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
