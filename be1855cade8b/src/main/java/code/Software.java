df7c543dbdbb20ea55cda38ee6cac72264a147ad
Adison Jimenez <adisonzenemij@gmail.com>
2025-05-05 20:35:32 -0500

    feat: actualizar artifactId y agregar nombre en pom.xml; limpiar comentarios en Software.java

--- be1855cade8b/src/main/java/code/Software.java
+++ be1855cade8b/src/main/java/code/Software.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package code;
 
-/**
- *
- * @author adiso
- */
 public class Software {
 
     public static void main(String[] args) {
